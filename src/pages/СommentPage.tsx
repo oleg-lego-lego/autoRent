7d14834f215f745cbe58c1090c2a311182df42f8
oleg ostrovski <oleg.ostrovski@gmail.com>
2023-06-26 15:53:56 +0300

    created component CommentBlog

--- src/pages/СommentPage.tsx
+++ src/pages/СommentPage.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import {HeroPages} from "../components/HeroPages";
 import {Footer} from "../components/Footer";
+import {CommentsBlog} from "../components/comment/CommentBlog";
 
 export const CommentPage = () => {
     return (
         <>
             <section className="testimonial-page">
                 <HeroPages name={'Comment'}/>
+                <CommentsBlog />
                 <div className="book-banner">
                     <div className="book-banner__overlay"></div>
                     <div className="container">
