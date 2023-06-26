0ad1fba1c692b08422b714472110d544a0beb40d
oleg ostrovski <oleg.ostrovski@gmail.com>
2023-06-26 15:38:38 +0300

    added description to component CommentPage

--- src/pages/СommentPage.tsx
+++ src/pages/СommentPage.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import {HeroPages} from "../components/HeroPages";
+import {Footer} from "../components/Footer";
 
 export const CommentPage = () => {
     return (
-        <div>
-
-        </div>
+        <>
+            <section className="testimonial-page">
+                <HeroPages name={'Comment'}/>
+                <div className="book-banner">
+                    <div className="book-banner__overlay"></div>
+                    <div className="container">
+                        <div className="text-content">
+                            <h2>Book a car by getting in touch with us</h2>
+                            <span>
+                                <i className="fa-solid fa-phone"></i>
+                                <h3>(123) 456-7869</h3>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <Footer/>
+            </section>
+        </>
     );
 };
 
