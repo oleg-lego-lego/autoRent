912f9c65c5fb82a35bcfc4d176b5510eed9aa8be
oleg ostrovski <oleg.ostrovski@gmail.com>
2023-07-04 14:40:12 +0300

    add CommentsBlog components for home page

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {PlanTrip} from "../about/PlanTrip";
+import {CommentsBlog} from "../comment/CommentBlog";
 
 export const Home = () => {
     return (
         <>
             <PlanTrip/>
+            <CommentsBlog/>
         </>
     );
 };
