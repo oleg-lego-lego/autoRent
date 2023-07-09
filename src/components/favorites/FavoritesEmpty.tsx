94f66da47080b902d8df177c5be54824081e587d
oleg ostrovski <oleg.ostrovski@gmail.com>
2023-07-09 14:40:29 +0300

    add style css for FavoritesEmpty

--- src/components/favorites/FavoritesEmpty.tsx
+++ src/components/favorites/FavoritesEmpty.tsx
@@ -1,23 +1,21 @@
 import React from 'react';
-import FavEmpty from "../../images/FavoritesEmpty.png"
+import FavEmpty from "../../images/emptyFavotites.png"
 import {NavbarLink} from "../navbar/NavbarLink";
 import {PATH} from "../../App";
 
 export const FavoritesEmpty = () => {
     return (
         <div className="container">
-            <div className="favorites--emp">
+            <div className="favorites-section">
                 <div className="favorites--empty">
-                    <h2>Корзина пустая
-                        <span>😕</span>
+                    <h2>Cart of favorite cars is empty.
+                        <span> 😕</span>
                     </h2>
-                    <p>Вероятней всего, вы не заказывали ещё пиццу.
-                        <br/>
-                        Для того, чтобы заказать пиццу,
-                        <NavbarLink title={'перейди на главную страницу.'} to={PATH.HOME} className={''}/>
+                    <p>You probably didn't choose a car.
+                        In order to take the car you like, go to page...
+                        <NavbarLink title={'Car Models'} to={PATH.MODELS} className={'favorites--empty-link'}/>
                     </p>
                     <img src={FavEmpty} alt="Empty favorites"/>
-
                 </div>
             </div>
         </div>
