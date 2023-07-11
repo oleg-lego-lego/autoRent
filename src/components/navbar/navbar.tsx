7ec53f90fa069aba97149bb78a9cbe948fb24259
oleg ostrovski <oleg.ostrovski@gmail.com>
2023-07-11 14:45:18 +0300

    refactoring picture in project

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from "../../images/carLogo.png"
+import logo from "../../images/logo/carLogo.png"
 import {Link} from "react-router-dom";
 import {NavbarLink} from "./NavbarLink";
 import {PATH} from "../../App";
