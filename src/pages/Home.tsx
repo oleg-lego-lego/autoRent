6922db254e114ac4642fea6bfa4c7b558834c546
oleg ostrovski <oleg.ostrovski@gmail.com>
2023-07-11 14:35:00 +0300

    refactoring front code for Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import {PlanTrip} from "../components/about/PlanTrip";
-import {CommentsBlog} from "../components/comment/CommentBlog";
 import {SelectCar} from "../components/home/SelectCar";
 import {TakeUs} from "../components/home/TakeUs";
 import {Download} from "../components/home/Download";
@@ -10,11 +8,9 @@ import {Promo} from "../components/home/Promo";
 export const Home = () => {
     return (
         <>
-            <PlanTrip/>
-            <CommentsBlog/>
-            <TakeUs/>
             <SelectCar/>
             <Promo/>
+            <TakeUs/>
             <Faq/>
             <Download/>
         </>
