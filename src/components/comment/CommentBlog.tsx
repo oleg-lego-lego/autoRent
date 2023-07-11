d8e8d90ba0aeeb44d986c39a86bba549774f2df1
oleg ostrovski <oleg.ostrovski@gmail.com>
2023-07-11 14:27:14 +0300

    add id for CommentType, + key in comment.map

--- src/components/comment/CommentBlog.tsx
+++ src/components/comment/CommentBlog.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import Img3 from "../../images/testimonials/pfp2.jpg";
 import {Comment} from "./Comment";
 import Img2 from "../../images/testimonials/pfp1.jpg";
+import {v1} from "uuid";
 
 export type CommentType = {
+    id: string
     comment: string
     name: string
     city: string
@@ -12,12 +14,14 @@ export type CommentType = {
 
 const comment: CommentType[] = [
     {
+        id: v1(),
         comment: 'We rented a car from this website and had an amazing experience! The booking was easy and the rental rates were very affordable.',
         name: 'Parry Hotter',
         city: 'Belgrade',
         img: Img2
     },
     {
+        id: v1(),
         comment: 'The car was in great condition and made our trip even better. Highly recommend for this car rental website!',
         name: 'Ron Rizzly',
         city: 'Novi Sad',
@@ -45,7 +49,7 @@ export const CommentsBlog = () => {
                         <div className="all-testimonials">
                             {comment.map(el => {
                                 return (
-                                    <Comment comment={el}/>
+                                    <Comment comment={el} key={el.id}/>
                                 )
                             })}
                         </div>
