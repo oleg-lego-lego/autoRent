3404b78eceb29c35335b5a0d09258d965ecfb573
oleg ostrovski <oleg.ostrovski@gmail.com>
2023-09-20 14:43:45 +0300

    add HeroPages in Login.tsx

--- src/components/login_account/Login.tsx
+++ src/components/login_account/Login.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import {LoginAccount} from "./LoginAccount";
+import {HeroPages} from "../HeroPages";
 
 export const Login = () => {
     return (
         <>
+            <HeroPages name={'Login'}/>
             <LoginAccount/>
         </>
     );
