7865d5fc6a6d702d7eb47afe7f001cfc140b5934
oleg ostrovski <oleg.ostrovski@gmail.com>
2023-09-28 14:59:15 +0300

    add logic, get model carList from API

--- src/pages/Models.tsx
+++ src/pages/Models.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {HeroPages} from "../components/HeroPages";
 import {Model} from "../components/Model";
-import {useAppSelector} from "../hooks/redux";
+import {useAppDispatch, useAppSelector} from "../hooks/redux";
+import {carsApi} from "../api/cars-api";
+import {getCars} from "../app/reducer/carModels-reducer";
 
 
 export const Models = () => {
+    const dispatch = useAppDispatch()
     const carModels = useAppSelector(state => state.carModels.items)
+
+    useEffect(() => {
+        carsApi.getCars()
+            .then((res) => {
+                dispatch(getCars(res.data))
+            })
+    }, [dispatch])
 
     return (
         <section>
