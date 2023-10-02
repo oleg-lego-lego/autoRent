2a7c0b28cfe93d53d81db5e87a50468709dc18da
oleg ostrovski <oleg.ostrovski@gmail.com>
2023-10-02 15:22:16 +0300

    add put a request for getFavorites car

--- src/api/cars-api.ts
+++ src/api/cars-api.ts
@@ -1,8 +1,15 @@
 import axios from "axios";
 import {CarModelsType} from "../app/reducer/carModels";
 
+const instance = axios.create({
+    baseURL: 'https://6512a403b8c6ce52b395f2d4.mockapi.io/api/carRent/',
+})
+
 export const carsApi = {
     getCars() {
-        return axios.get<CarModelsType>('https://6512a403b8c6ce52b395f2d4.mockapi.io/api/carRent/cars/')
+        return instance.get<CarModelsType>('/cars/')
+    },
+    getFavorites(id: string, favorites: boolean) {
+        return instance.put<CarModelsType>(`cars/${id}`, {favorites})
     }
 }
