030c7ac7c53a4d2a04469ee211ce7a563726cf93
oleg ostrovski <oleg.ostrovski@gmail.com>
2023-10-08 18:21:52 +0300

    add HeroPages component

--- src/components/notFound/NotFound.tsx
+++ src/components/notFound/NotFound.tsx
@@ -2,22 +2,27 @@ import React from 'react';
 import {PATH} from "../../App";
 import notFound from "../../images/notFound.png";
 import {TemplatePage} from "../TemplatePage";
+import {HeroPages} from "../HeroPages";
 
 export const NotFound = () => {
     const header = 'Web page not found.';
     const description = 'It looks like the page you are looking for has disappeared! go to page...';
 
     return (
-        <TemplatePage
-            header={header}
-            description={description}
-            linkTitle={'Home'}
-            path={PATH.HOME}
-            linkClassName={'favorites--empty-link'}
-            img={notFound}
-            imgDescription={'Empty favorites'}
-            imgClassName={'notFound_img'}
-        />
+        <>
+            <HeroPages name={'Favorites'}/>
+            <TemplatePage
+                header={header}
+                description={description}
+                linkTitle={'Home'}
+                path={PATH.HOME}
+                linkClassName={'favorites--empty-link'}
+                img={notFound}
+                imgDescription={'Empty favorites'}
+                imgClassName={'notFound_img'}
+            />
+        </>
+
     );
 };
 
