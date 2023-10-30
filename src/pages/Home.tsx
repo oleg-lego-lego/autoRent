470d9f3e972748a93d882d4a5e7d21b527af2f77
oleg ostrovski <oleg.ostrovski@gmail.com>
2023-10-30 11:51:29 +0300

    add logic page Home for get getUserInLogged

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SelectCar} from "../components/home/SelectCar";
 import {TakeUs} from "../components/home/TakeUs";
 import {Download} from "../components/home/download/Download";
@@ -6,8 +6,20 @@ import {Faq} from "../components/home/fac/Faq";
 import {Promo} from "../components/home/Promo";
 import {BookCar} from "../components/home/book_car/BookCar";
 import {HomeHeader} from "../components/home/homeHeader/HomeHeader";
+import {useAppDispatch} from "../hooks/redux";
+import {carsApiLogin} from "../api/cars-api";
+import {getAuthUser} from "../app/reducer/auth-reducer";
 
 export const Home = () => {
+    const dispatch = useAppDispatch()
+
+    useEffect(() => {
+        carsApiLogin.getUserInLogged()
+            .then(res => {
+                dispatch(getAuthUser(res.data))
+            })
+    }, [dispatch])
+
     return (
         <>
             <HomeHeader/>
