631409163e7d534011257c38d46fceed42051fee
oleg ostrovski <oleg.ostrovski@gmail.com>
2023-11-10 20:43:32 +0300

    add fetchGetCars and fetchPutFavorites in carModels-reducer.ts

--- src/app/reducer/carModels-reducer.ts
+++ src/app/reducer/carModels-reducer.ts
@@ -1,5 +1,53 @@
-import {createSlice} from '@reduxjs/toolkit'
+import {createAsyncThunk, createSlice} from '@reduxjs/toolkit'
 import {CarModelsType} from "./carModels";
+import {isLoading} from "./isLoading-reducer";
+import {carsApi} from "../../api/cars-api";
+import {setErrorSnackbar} from "./error-reducer";
+
+
+export const fetchGetCars = createAsyncThunk('cars/fetchGetCars',
+    async (_, thunkAPI) => {
+
+    thunkAPI.dispatch(isLoading('loading'));
+
+    try {
+        const res = await carsApi.getCars();
+        setTimeout(() => {
+            thunkAPI.dispatch(getCars(res.data))
+        }, 3000)
+
+        return res.data;
+    } catch (error) {
+        thunkAPI.dispatch(setErrorSnackbar(error))
+        console.error('Произошла ошибка:', error)
+
+        throw error;
+    } finally {
+        setTimeout(() => {
+            thunkAPI.dispatch(isLoading('idle'))
+        }, 3000)
+    }
+});
+
+export const fetchPutFavorites = createAsyncThunk('cars/fetchPutFavorites',
+    async ({ id, favorites }: { id: string, favorites: boolean }, thunkAPI) => {
+
+        thunkAPI.dispatch(isLoading('loading'));
+
+        try {
+            const res = await carsApi.putFavorites(id, favorites)
+            thunkAPI.dispatch(carModelsFavorites(res.data))
+        } catch (error) {
+            thunkAPI.dispatch(setErrorSnackbar(error))
+            console.error('Произошла ошибка:', error)
+        } finally {
+            setTimeout(() => {
+                thunkAPI.dispatch(isLoading('idle'))
+            }, 3000)
+        }
+    });
+
+
 
 export interface CarModelsStateType {
     items: CarModelsType[]
