ab603392c588d9f12dba7e3e4e6be35e3fbc8be5
oleg ostrovski <oleg.ostrovski@gmail.com>
2023-11-13 12:14:18 +0300

    add logic skeleton

--- src/pages/Favotites.tsx
+++ src/pages/Favotites.tsx
@@ -1,28 +1,50 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Model} from "../components/Model";
 import {HeroPages} from "../components/HeroPages";
 import {useAppSelector} from "../hooks/redux";
 import {FavoritesEmpty} from "../components/favorites/FavoritesEmpty";
+import {CarCardSkeleton} from "../components/skeleton/CarCardSreleton";
 
 
 export const Favorites = () => {
+    const [delayComplete, setDelayComplete] = useState(false)
+
     const carModel = useAppSelector(state => state.carModels.items)
     const carModelFavorites = carModel.filter(el => el.favorites !== false)
+
+    useEffect(() => {
+        const delay = setTimeout(() => {
+            setDelayComplete(true);
+        }, 3000);
+
+        return () => clearTimeout(delay);
+    }, [carModelFavorites]);
+
+    const carCardsSkeleton = (
+        <div className="models-div">
+            {[1, 2].map(el => <CarCardSkeleton key={el}/>)}
+        </div>
+    )
+
+    const carFavorites = (
+        <div className="models-div">
+            {carModelFavorites.map(el => (
+                <Model model={el} key={el.id}/>
+            ))}
+        </div>
+    )
 
     return (
         <>
             <HeroPages name={'Favorites'}/>
             <div className="container">
-                {!carModelFavorites.length
-                    ? <FavoritesEmpty/>
-                    : <div className="models-div">
-                        {carModelFavorites.map(el => {
-                            return (
-                                <Model model={el} key={el.id}/>
-                            )
-                        })}
-                    </div>
-                }
+                {!delayComplete && carCardsSkeleton}
+
+                {delayComplete && (
+                    carModelFavorites.length
+                        ? carFavorites
+                        : <FavoritesEmpty/>
+                )}
             </div>
         </>
     );
