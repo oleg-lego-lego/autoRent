bcb1a61eae931d8eeb722a30d02d677b74906bf5
oleg ostrovski <oleg.ostrovski@gmail.com>
2023-11-14 13:53:14 +0300

    add disabled in isLoading-reducer.ts

--- src/app/reducer/isLoading-reducer.ts
+++ src/app/reducer/isLoading-reducer.ts
@@ -3,11 +3,13 @@ import {createSlice} from '@reduxjs/toolkit'
 export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
 
 export interface LoadingStateType {
-    status: RequestStatusType
+    status: RequestStatusType,
+    disabled: boolean,
 }
 
 const initialState: LoadingStateType = {
-    status: 'idle'
+    status: 'idle',
+    disabled: false,
 };
 
 export const IsLoadingSlice = createSlice({
@@ -17,9 +19,12 @@ export const IsLoadingSlice = createSlice({
         isLoading: (state, action) => {
             state.status = action.payload
         },
+        isDisabledButton: (state, action) => {
+            state.disabled = action.payload
+        },
     },
 })
 
-export const {isLoading} = IsLoadingSlice.actions
+export const {isLoading, isDisabledButton} = IsLoadingSlice.actions
 
 export default IsLoadingSlice.reducer
